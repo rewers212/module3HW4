0f401089cb09d12900c03cde8b0e1c1d1cfbe4c4
ABC\User <rewers212@gmail.com>
2021-08-06 22:00:10 +0300

    HW

---
+++ Module3HW4/Module3HW4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module3HW4
+{
+    public class Program
+    {
+        private static Func<int, int, int> _rezSumHandler;
+        public static void Main(string[] args)
+        {
+            var rez = 0;
+            TryCatch(() =>
+            {
+                rez = Zamikan();
+            });
+            Console.WriteLine(rez);
+            var h = new HWLINQ();
+            h.Linq();
+        }
+
+        public static void TryCatch(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static int Zamikan()
+        {
+            _rezSumHandler += (x, y) => x + y;
+            _rezSumHandler += (x, y) => x + y;
+            var rezultat = 0;
+            var result = _rezSumHandler.GetInvocationList();
+            foreach (var item in result)
+            {
+                var resultInMethod = item.DynamicInvoke(5, 9);
+                rezultat += (int)resultInMethod;
+            }
+
+            return rezultat;
+        }
+    }
+}
